925929ee96ca60c9552b1d6f831c74913184a03d
LiXuemin <lxmheida@gmail.com>
2021-05-07 14:45:54 +0800

    feat: multithread demo

---
+++ multithread-demo/src/main/java/com/lixuemin/thread/VolatileDemo.java
@@ -0,0 +1,36 @@
+package com.lixuemin.thread;
+
+/**
+ * @program: demo-for-someframework
+ *
+ * @description:
+ *
+ * @author: lixuemin
+ *
+ * @create: 2021-04-28
+ **/
+public class VolatileDemo {
+    private static volatile int x = 1;
+
+    public static void main(String[] args) {
+        System.out.println(1^5^5);
+    }
+    public static void main1(String[] args) throws InterruptedException {
+
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+               x = i;
+            }
+        });
+
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                System.out.println(x);
+            }
+        });
+
+        t1.start();
+        t2.start();
+        Thread.sleep(2000);
+    }
+}
