2df90f0e165935c282fd9400f32e4f18b997fe9a
LiXuemin <lxmheida@gmail.com>
2021-06-08 08:55:44 +0800

    algo demo

--- multithread-demo/src/main/java/com/lixuemin/thread/StreamForeachDemo.java
+++ multithread-demo/src/main/java/com/lixuemin/thread/StreamForeachDemo.java
@@ -3,6 +3,7 @@ package com.lixuemin.thread;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 /**
@@ -19,6 +20,21 @@ public class StreamForeachDemo {
         List<Integer> list = new ArrayList(Arrays.asList(1,2,3,4,5,6));
         list.forEach(StreamForeachDemo::incr);
         System.out.println(list.toString());
+
+        List<QuestionVO> questionVOList = new ArrayList<>();
+        questionVOList.stream().flatMap(q -> Arrays.stream(q.getKnowledge())).collect(Collectors.toList());
+    }
+
+    public class QuestionVO {
+        private String[] knowledge;
+
+        public String[] getKnowledge() {
+            return knowledge;
+        }
+
+        public void setKnowledge(String[] knowledge) {
+            this.knowledge = knowledge;
+        }
     }
 
     private static void incr(Integer s) {
