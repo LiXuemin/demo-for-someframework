2030ec95462adab373bd3eed02e1db532973429b
LiXuemin <lixuemin@beiwaionline.com>
2021-08-25 16:39:05 +0800

    feat: add LRUCache.java

--- algorithm-demo/src/main/java/com/lixuemin/algo/LRUCache.java
+++ algorithm-demo/src/main/java/com/lixuemin/algo/LRUCache.java
@@ -1,32 +1,30 @@
 package com.lixuemin.algo;
 
 import java.util.LinkedHashMap;
+import java.util.Map;
 
 
 /**
  *
  **/
-class LRUCache {
-    private LinkedHashMap<Integer, Integer> map;
+class LRUCache extends LinkedHashMap<Integer, Integer>{
     private int capacity;
 
     public LRUCache(int capacity) {
-        map = new LinkedHashMap<>(capacity);
+        super(capacity, 0.75F, true);
         this.capacity = capacity;
     }
 
     public int get(int key) {
-        return map.getOrDefault(key, -1);
+        return super.getOrDefault(key, -1);
     }
 
     public void put(int key, int value) {
-        if (map.size() == capacity) {
-            removeLRU();
-        }
-        map.put(key, value);
+        super.put(key, value);
     }
 
-    private void removeLRU() {
-
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+        return size() > capacity;
     }
 }
