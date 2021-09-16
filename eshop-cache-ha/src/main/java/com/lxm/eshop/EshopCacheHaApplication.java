665ef055612402d5dadd7ce49ad48a17bc059c44
LiXuemin <lixuemin@beiwaionline.com>
2021-09-16 16:09:04 +0800

    feat: 初始提交eshop demo

---
+++ eshop-cache-ha/src/main/java/com/lxm/eshop/EshopCacheHaApplication.java
@@ -0,0 +1,13 @@
+package com.lxm.eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class EshopCacheHaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EshopCacheHaApplication.class, args);
+    }
+}
---
+++ eshop-cache-ha/src/test/java/com/lxm/eshop/cache/ha/EshopCacheHaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxm.eshop.cache.ha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class EshopCacheHaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
