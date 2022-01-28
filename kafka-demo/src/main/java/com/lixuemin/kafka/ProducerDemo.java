efe89c2c24e7c1826bfbfc8b0cf46e8019df9510
LiXuemin <lixuemin@beiwaionline.com>
2022-01-28 10:17:04 +0800

    feat: kafka producer simple demo

--- kafka-demo/src/main/java/com/lixuemin/kafka/ProducerDemo.java
+++ kafka-demo/src/main/java/com/lixuemin/kafka/ProducerDemo.java
@@ -1,22 +1,36 @@
 package com.lixuemin.kafka;
 
 import java.util.Properties;
+import java.util.concurrent.CompletableFuture;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 
 /**
  *
  **/
 public class ProducerDemo {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProducerDemo.class);
+
     public static void main(String[] args) {
+
         Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092");
+        props.put("bootstrap.servers", "127.0.0.1:9092");
+        props.put("acks", "all");
+        props.put("retries", 0);
+        props.put("linger.ms", 1);
         props.put("key.serializer", StringSerializer.class.getName());
         props.put("value.serializer", StringSerializer.class.getName());
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(props);
-        producer.send(new ProducerRecord<String, String>("quickstart-events", "send from idea"));
-        producer.close();
+        try {
+            producer.send(new ProducerRecord<String, String>("quickstart-events", "send from idea"));
+        } catch (Exception e) {
+            LOGGER.error("send message error", e);
+        } finally {
+            producer.close();
+        }
     }
 }
