a3afe0794d6d085fb128a615d4052e38d5e8f133
LiXuemin <lixuemin@beiwaionline.com>
2022-02-18 17:55:52 +0800

    feat: add kafka producerInterceptor demo

---
+++ kafka-demo/src/main/java/com/lixuemin/kafka/producer/DemoInterceptor.java
@@ -0,0 +1,32 @@
+package com.lixuemin.kafka.producer;
+
+import java.util.Map;
+import org.apache.kafka.clients.producer.ProducerInterceptor;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+
+/**
+ *
+ **/
+public class DemoInterceptor implements ProducerInterceptor {
+    @Override
+    public ProducerRecord onSend(ProducerRecord record) {
+        return null;
+    }
+
+    @Override
+    public void onAcknowledgement(RecordMetadata metadata, Exception exception) {
+
+    }
+
+    @Override
+    public void close() {
+
+    }
+
+    @Override
+    public void configure(Map<String, ?> configs) {
+
+    }
+}
