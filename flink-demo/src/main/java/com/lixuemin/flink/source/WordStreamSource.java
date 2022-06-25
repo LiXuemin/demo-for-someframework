c822b611ec7876556fd3383c14a0575dbbec392f
lixuemin <lxmheida@gmail.com>
2022-06-25 11:44:00 +0800

    refactor: code rafactor

--- flink-demo/src/main/java/com/lixuemin/flink/source/WordStreamSource.java
+++ flink-demo/src/main/java/com/lixuemin/flink/source/WordStreamSource.java
@@ -7,8 +7,9 @@ import org.apache.flink.streaming.api.functions.source.SourceFunction;
 
 
 public class WordStreamSource implements SourceFunction<String> {
-    private final long count = 1L;
     private boolean running = true;
+
+    private final Random random = new Random();
 
     @Override
     public void run(SourceContext<String> sourceContext) throws Exception {
@@ -22,7 +23,7 @@ public class WordStreamSource implements SourceFunction<String> {
             list.add("sql");
             list.add("hello");
             int size = list.size();
-            int i = new Random().nextInt(size);
+            int i = random.nextInt(size);
             sourceContext.collect(list.get(i));
             Thread.sleep(1000);
         }
