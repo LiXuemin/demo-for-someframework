ff161d6a2a30726dc6b21adbd363758cda6d6984
LiXuemin <lixuemin@beiwaionline.com>
2022-09-05 09:58:19 +0800

    feat: add demo of algo

---
+++ algorithm-demo/src/main/java/com/lixuemin/algo/PowerOfFour.java
@@ -0,0 +1,14 @@
+package com.lixuemin.algo;
+
+/**
+ *
+ **/
+public class PowerOfFour {
+    public static void main(String[] args) {
+        System.out.println(9&3);
+        System.out.println(3&3);
+        System.out.println(4&3);
+        System.out.println(3&2);
+        System.out.println(9&8);
+    }
+}
