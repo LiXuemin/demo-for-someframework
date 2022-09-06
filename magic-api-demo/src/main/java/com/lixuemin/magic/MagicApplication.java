c97d7a810725b7dd6e153801920647debcb72e9e
LiXuemin <lixuemin@beiwaionline.com>
2022-09-06 16:45:53 +0800

    feat: add demo of magic-api

---
+++ magic-api-demo/src/main/java/com/lixuemin/magic/MagicApplication.java
@@ -0,0 +1,18 @@
+package com.lixuemin.magic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @description bootstrap application
+ * @author: lixuemin
+ * @date: 2022-09-05
+ **/
+@SpringBootApplication
+public class MagicApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MagicApplication.class, args);
+    }
+
+}
